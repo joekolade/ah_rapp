95f49587a00eb53d5773593d597899df0d2b0557
Joe Schäfer <schaefer@scw.info>
2017-04-04 22:16:23 +0200

    Initial TS

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -1,1 +1,1 @@
-<INCLUDE_TYPOSCRIPT: source="FILE: EXT:ah_rapp/Configuration/TypoScript/bricks/contatc_form_setup.ts">
+<INCLUDE_TYPOSCRIPT: source="FILE: EXT:ah_rapp/Configuration/TypoScript/bricks/contact_form_setup.ts">
