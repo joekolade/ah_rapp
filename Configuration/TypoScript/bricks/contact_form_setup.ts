398882c8a15a55950c9c829f650c9b4d2c6d5364
Joe Schäfer <schaefer@scw.info>
2017-04-04 22:55:57 +0200

    Contactform start

--- Configuration/TypoScript/bricks/contact_form_setup.ts
+++ Configuration/TypoScript/bricks/contact_form_setup.ts
@@ -7,4 +7,14 @@ lib {
 }
 
 lib.contactbar = COA
+lib.contactbar {
+    wrap = <div class="panel panel-info" style="margin-top:1em;"><div class="panel-body"><div class="row">|</div></div></div>
+}
+lib.contactbar.10 {
+    wrap = <div class="col-sm-6">|</div>
+}
+
 lib.contactbar.20 < lib.contactform
+lib.contactbar.20 {
+    wrap = <div class="col-sm-6">|</div>
+}
