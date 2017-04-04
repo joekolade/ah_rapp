844d82b388b43539b991263fa2264113d9f1f254
Joe Schäfer <schaefer@scw.info>
2017-04-04 22:27:15 +0200

    Contactform start

--- Configuration/TypoScript/bricks/contact_form_setup.ts
+++ Configuration/TypoScript/bricks/contact_form_setup.ts
@@ -1,3 +1,12 @@
 page.10.variables {
-    contactform <
+    contactform = FLUIDTEMPLATE
+    contactform {
+        file = EXT:ah_rapp/Resources/Private/Templates/Contactform.html
+
+        variables {
+            form < styles.content.get
+            form.select.pidInList = {$site.ah_rapp.contactform.pid}
+            form.select.uidInList = {$site.ah_rapp.contactform.ceuid}
+        }
+    }
 }
---
+++ Configuration/TypoScript/constants.ts
@@ -0,0 +1,9 @@
+
+// Site "Namespace"
+//
+
+site.ah_rapp {
+
+    contactform.pid = 95
+    contactform.ceuid =
+}
