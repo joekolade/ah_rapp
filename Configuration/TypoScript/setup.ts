97bb658189c698dcdcbab4a2c3165043e1a4fa5c
Joe Schäfer <schaefer@scw.info>
2017-04-06 22:48:37 +0200

    Powermail stylo

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -1,1 +1,7 @@
+page {
+    includeCSS {
+        ah_rapp_custom = EXT:ah_rapp/Resources/Public/Css/ahrapp_custom.css
+    }
+}
+
 <INCLUDE_TYPOSCRIPT: source="FILE: EXT:ah_rapp/Configuration/TypoScript/bricks/contact_form_setup.ts">
