950c1daf1ecd43c1d52c8785c0f2827e9c17d981
Joe Schäfer <schaefer@scw.info>
2017-05-19 11:12:23 +0200

    responsive images

--- Configuration/TypoScript/bricks/resp_images.ts
+++ Configuration/TypoScript/bricks/resp_images.ts
@@ -1,7 +1,28 @@
 tt_content.image.20.1 {
     layoutKey = srcset
 
-    params = sizes="(min-width: 700px) 700px, 100vw"
+    # bootstrap package does this
+
+    params.cObject = CASE
+    params.cObject {
+        key.field = layout
+        # Default is responsive
+        default = TEXT
+        default.value = class="img-responsive img-content" sizes="(min-width: 1140px) 1600px, (min-width: 992px) 1140px, (min-width: 768px) 992px, (min-width: 480px) 768px, (min-width: 320px) 480px, auto"
+        # Normal
+        1 = TEXT
+        1.value = class="img-content" sizes="(min-width: 1140px) 1600px, (min-width: 992px) 1140px, (min-width: 768px) 992px, (min-width: 480px) 768px, (min-width: 320px) 480px, auto"
+        # Rounded corners
+        2 = TEXT
+        2.value = class="img-rounded img-content" sizes="(min-width: 1140px) 1600px, (min-width: 992px) 1140px, (min-width: 768px) 992px, (min-width: 480px) 768px, (min-width: 320px) 480px, auto"
+        3 = TEXT
+        3.value = class="img-rounded img-responsive img-content" sizes="(min-width: 1140px) 1600px, (min-width: 992px) 1140px, (min-width: 768px) 992px, (min-width: 480px) 768px, (min-width: 320px) 480px, auto"
+        # Circle
+        4 = TEXT
+        4.value = class="img-circle img-content" sizes="(min-width: 1140px) 1600px, (min-width: 992px) 1140px, (min-width: 768px) 992px, (min-width: 480px) 768px, (min-width: 320px) 480px, auto"
+        5 = TEXT
+        5.value = class="img-circle img-responsive img-content" sizes="(min-width: 1140px) 1600px, (min-width: 992px) 1140px, (min-width: 768px) 992px, (min-width: 480px) 768px, (min-width: 320px) 480px, auto"
+    }
 
     # Default Breakpoints
     sourceCollection >
@@ -12,6 +33,7 @@ tt_content.image.20.1 {
             srcsetCandidate = 1600w
             sizes = (min-width: 1140px)
         }
+
         xl {
             # dataKey = extra-large
             maxW = 1140
