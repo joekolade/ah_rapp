924334b8817b495ba5c5ad06c1a10af2e00dbda8
Joe Schäfer <schaefer@scw.info>
2017-05-19 10:45:24 +0200

    responsive images

--- Configuration/TypoScript/bricks/resp_images.ts
+++ Configuration/TypoScript/bricks/resp_images.ts
@@ -38,79 +38,42 @@
 #    }
 #}
 
-tt_content.image.20.1{
-    layoutKey = srcset
-    layout {
-        default {
-            element = <img src="###SRC###" width="###WIDTH###" height="###HEIGHT###" ###PARAMS### ###ALTPARAMS### ###BORDER### ###SELFCLOSINGTAGSLASH###>
-        }
-        srcset {
-            element = <img src="###SRC###" srcset="###SOURCECOLLECTION###" ###PARAMS### ###ALTPARAMS### ###SELFCLOSINGTAGSLASH###>
-            source = |*|###SRC### ###SRCSETCANDIDATE###,|*|###SRC### ###SRCSETCANDIDATE###
-        }
-        picture {
-            element = <picture> ###SOURCECOLLECTION### <img src="###SRC###" ###PARAMS### ###ALTPARAMS### ###SELFCLOSINGTAGSLASH###></picture>
-            source = <source srcset="###SRC###" media="###MEDIAQUERY###" ###SELFCLOSINGTAGSLASH###>
-        }
-        data {
-            element = <img src="###SRC###" ###SOURCECOLLECTION### ###PARAMS### ###ALTPARAMS### ###SELFCLOSINGTAGSLASH###>
-            source = data-###DATAKEY###="###SRC###"
-        }
-        yourCustomRendering {
-            element = //your element definition
-            source = // your source definition
-        }
-    }
-}
-/*
-tt_content.image.20.1.sourceCollection >
-tt_content.image.20.1.sourceCollection {
-big {
-maxW = 600
-dataKey = big
-}
-small {
-maxW = 200
-dataKey = small
-}
-smallLandscape {
-maxW = 400
-dataKey = smallLandscape
-}
-}
-*/
+
 
 tt_content.image.20.1 {
+    layoutKey = srcset
+
+    # Default Breakpoints
     sourceCollection {
-        small {
-            // […]
+        xl {
+            # dataKey = extra-large
+            maxW = 1200
+            srcsetCandidate = 1200w
         }
-        medium {
-            width = 400
-            height = 300
-            maxW = 400
-            maxH = 300
-            minW = 400
-            minH = 300
-            quality = 90
-            pixelDensity = 1
-            srcsetCandidate = 800w
-            mediaQuery = (min-device-width: 800px)
-            dataKey = medium
+
+        lg {
+            # data-key = large
+            maxW = 992
+            srcsetCandidate = 992w
         }
-        mediumHires {
-            width = 400
-            maxW = 400
-            pixelDensity = 2
-            srcsetCandidate = 400w 2x
-            mediaQuery =
-            dataKey = mediumHires
+
+        md {
+            # data-key = medium
+            maxW = 768
+            srcsetCandidate = 768w
         }
-        large {
-            // […]
+
+        sm {
+            # data-key = small
+            maxW = 480
+            srcsetCandidate = 480w
         }
-        monster {
-            // […]
+
+        xs {
+            # dataKey = extra-small
+            maxW = 320
+            srcsetCandidate = 320w
+            sizes = auto
         }
     }
 }
