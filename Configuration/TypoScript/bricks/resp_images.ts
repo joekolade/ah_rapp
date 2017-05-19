3336ad53195c45dc408812163c4cf231c138162a
Joe Schäfer <schaefer@scw.info>
2017-05-19 11:02:58 +0200

    responsive images

--- Configuration/TypoScript/bricks/resp_images.ts
+++ Configuration/TypoScript/bricks/resp_images.ts
@@ -9,41 +9,41 @@ tt_content.image.20.1 {
         huge {
             # dataKey = extra-large
             maxW = 1600
-            srcsetCandidate = 1600px
+            srcsetCandidate = 1600w
             sizes = (min-width: 1140px)
         }
         xl {
             # dataKey = extra-large
             maxW = 1140
-            srcsetCandidate = 1140px
+            srcsetCandidate = 1140w
             sizes = (min-width: 992px)
         }
 
         lg {
             # data-key = large
             maxW = 992
-            srcsetCandidate = 992px
+            srcsetCandidate = 992w
             sizes = (min-width: 768px)
         }
 
         md {
             # data-key = medium
             maxW = 768
-            srcsetCandidate = 768px
+            srcsetCandidate = 768w
             sizes = (min-width: 480px)
         }
 
         sm {
             # data-key = small
             maxW = 480
-            srcsetCandidate = 480px
+            srcsetCandidate = 480w
             sizes = (min-width: 320px)
         }
 
         xs {
             # dataKey = extra-small
             maxW = 320
-            srcsetCandidate = 320px
+            srcsetCandidate = 320w
             sizes = auto
         }
     }
