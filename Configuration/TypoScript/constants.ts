674fe757719b25926847f5a4166f11509b30f4b1
Joe Schäfer <schaefer@scw.info>
2017-05-19 10:49:33 +0200

    responsive images

--- Configuration/TypoScript/constants.ts
+++ Configuration/TypoScript/constants.ts
@@ -13,7 +13,7 @@ ah_rapp {
 //
 
 styles.content.imgtext.responsive = 1
-styles.content.imgtext.layoutKey = srcset
+styles.content.imgtext.layoutKey = picture
 styles.content.textmedia.maxWInText = 1140
 
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:ah_rapp/Configuration/TypoScript/bricks/contact_form_constants.ts">
