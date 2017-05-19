454db10a6f2e075dfaec1302634cdb9d3a182c6c
Joe Schäfer <schaefer@scw.info>
2017-05-19 11:30:42 +0200

    scriptmerger

--- Configuration/TypoScript/constants.ts
+++ Configuration/TypoScript/constants.ts
@@ -9,6 +9,9 @@ ah_rapp {
     }
 }
 
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ah_rapp/Configuration/TypoScript/bricks/contact_form_constants.ts">
+
+
 // responsive images
 //
 
@@ -17,5 +20,17 @@ styles.content.imgtext.layoutKey = picture
 styles.content.imgtext.maxWInText = 1140
 styles.content.textmedia.maxWInText = 1140
 
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ah_rapp/Configuration/TypoScript/bricks/contact_form_constants.ts">
+
+// ext:scriptmerger
+//
+
+
+plugin.tx_scriptmerger.css.enable = 1
+plugin.tx_scriptmerger.css.compress.enable = 0
+plugin.tx_scriptmerger.css.merge.ignore = https://fonts.googleapis.com/css
+#plugin.tx_scriptmerger.css.compress.ignore = https://fonts.googleapis.com/css
+
+plugin.tx_scriptmerger.javascript.enable = 1
+plugin.tx_scriptmerger.javascript.compress.enable = 0
+plugin.tx_scriptmerger.javascript.parseBody = 1
 
