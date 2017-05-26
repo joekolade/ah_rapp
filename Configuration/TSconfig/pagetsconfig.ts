d37eef7c11223a09b9fc4c8c2c83d945ddb6d0b7
Joe Schäfer <schaefer@scw.info>
2017-05-27 00:17:12 +0200

    cs_Seo 4 news

--- Configuration/TSconfig/pagetsconfig.ts
+++ Configuration/TSconfig/pagetsconfig.ts
@@ -14,3 +14,26 @@ RTE.classesAnchor.internalLinkInNewWindow.titleText = Webseite öffnen
 RTE.classesAnchor.download.titleText = Datei herunterladen
 RTE.classesAnchor.downloadImage.titleText = Bild herunterladen
 RTE.classesAnchor.mail.titleText = E-Mail senden
+
+
+// Cs seo
+tx_csseo {
+    # new index and table name of the model
+    1 = tx_news_domain_model_news
+
+    # if the get parameter is set in the URL the cs_seo properties will be shown
+    1.enable = GP:tx_news_pi1|news
+
+    1.fallback {
+        title = title
+        description = teaser
+    }
+
+    #    1.evaluation {
+    #        # additional params to initialize the detail view, the pipe will be replaced by the uid
+    #        getParams = &tx_myext_pi1[controller]=MyController&tx_myext_pi1[action]=MyAction&tx_myext_pi1[mymod]=|
+    #
+    #        # detail pid for the current records, only if set the table will be available
+    #        detailPid =
+    #    }
+}
