9b2fbacba9f8bd4329796a5e123d64029a089d6d
Joe Schäfer <schaefer@scw.info>
2017-05-26 23:39:38 +0200

    RTE.classesAnchor

--- Configuration/TSconfig/pagetsconfig.ts
+++ Configuration/TSconfig/pagetsconfig.ts
@@ -2,3 +2,15 @@ TCAdefaults {
     # Bild oben links als Standard
     tt_content.imageorient = 2
 }
+
+
+/**
+* Title von Links
+*/
+RTE.classesAnchor.internalLink.titleText = Webseite anzeigen
+RTE.classesAnchor.externalLink.titleText = Webseite anzeigen
+RTE.classesAnchor.externalLinkInNewWindow.titleText = Webseite öffnen
+RTE.classesAnchor.internalLinkInNewWindow.titleText = Webseite öffnen
+RTE.classesAnchor.download.titleText = Datei herunterladen
+RTE.classesAnchor.downloadImage.titleText = Bild herunterladen
+RTE.classesAnchor.mail.titleText = E-Mail senden
