fcc10cc1e4a4ffbb6097a6f57506f2205ea78f1f
Joe Schäfer <schaefer@scw.info>
2017-05-30 23:00:44 +0200

    Facebook Button

---
+++ Configuration/TypoScript/bricks/navbar_extended_setup.ts
@@ -0,0 +1,6 @@
+lib.facebook = TEXT
+lib.facebook {
+    typolink.parameter = https://www.facebook.com/AutohausRapp
+    typolink.ATagParams = class="btn faceboooohoook pull-right"
+    value = <i class="fa fa-facebook"></i>
+}
